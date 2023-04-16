14d32c2bda87908d912ef3690bf8fb4cd97c25ef
avialp <121842983+avialp@users.noreply.github.com>
2023-04-16 15:38:19 -0400

    Add files via upload

---
+++ GDP.sql
@@ -0,0 +1,26 @@
+SELECT * 
+FROM gdp
+
+GROUP BY "country_code"
+
+SELECT MAX("2013") 
+from gdp
+
+SELECT MIN("2013") 
+from gdp
+
+SELECT COUNT("country_name") 
+from gdp 
+
+SELECT AVG("2013") 
+from gdp 
+
+SELECT MIN("2021") 
+FROM gdp 
+
+SELECT MAX("2021") 
+FROM gdp
+
+SELECT AVG("2021") 
+FROM gdp 
+
